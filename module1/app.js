2f7bbcce240cd63abcfb2a293141d6e1c4ceea84
arnaud <arnaud@something.com>
2016-11-28 18:31:37 +0100

    LunchCheck app update

--- module1/app.js
+++ module1/app.js
@@ -5,22 +5,43 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope){
-console.log($scope.lunchList);
-var arraysOfStrings = $scope.lunchList.split(',');
-var lunchListElt = arraysOfStrings.lenght;
-console.log(lunchListElt);
+$scope.lunchList = "";
+$scope.lunchListElt= 0;
+$scope.message= "";
 
-$scope.checklist = function(){
-// if (lunchListElement){
-//   $scope.message = "Please enter data first";
-// // }else if (){
-//
-// }
+$scope.displayNumberElt = function (){
+  var arraysOfStrings = splitArray($scope.lunchList);
+  $scope.lunchListElt = arraysOfStrings.length;
+  console.log($scope.lunchListElt);
+  var strMsg = displayMessage($scope.lunchListElt);
+  console.log(strMsg);
+  $scope.message = displayMessage($scope.lunchListElt);
+}
 
+function splitArray (string){
+  var cal=0;
+  return cal = string.split(',');
+}
 
+function displayMessage(elt){
+  var str="";
+  // elt.toInt();
+if(elt=1){
+  return str= "Enjoy!";
+}else if (elt=2) {
+  return str= "Enjoy!";
+}
+else if (elt=3) {
+  return str= "Enjoy!";
+}
+else if (elt>3) {
+  return str= "Too much!";
+}
+else return str;
 }
 
 }
+
 
 
 })();
