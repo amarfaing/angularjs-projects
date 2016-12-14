27a6c687223ce059c24c63fe1b6e5ca957896246
arnaud <arnaud@something.com>
2016-12-14 11:48:42 +0100

    fixed changes

--- module1/app.js
+++ module1/app.js
@@ -1,24 +1,24 @@
 (function(){
 'use strict';
+
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope){
-$scope.lunchList = null;
+$scope.lunchList = "";
 $scope.lunchListElt = 0;
-$scope.message = " ";
+$scope.message = "";
 
 $scope.display = function (){
   var arraysOfStrings = splitArray($scope.lunchList);
   $scope.lunchListElt = arraysOfStrings.length;
-  var strMsg = displayMessage($scope.lunchListElt);
   $scope.message = displayMessage($scope.lunchListElt);
 }
 
 function splitArray (string){
   var cal = 0;
-  if(string !== null && string !== ""){
+  if(string){
   return cal = string.split(',');
 }
 return cal;
@@ -27,21 +27,15 @@ return cal;
 function displayMessage(elt){
   var str = "";
 
-  if(elt===1){
-    return str = "Enjoy!";
-  }else if (elt===2) {
-    return str = "Enjoy!";
-  }
-  else if (elt===3) {
+  if(elt>0 & elt<3){
     return str = "Enjoy!";
   }
   else if (elt>3) {
     return str = "Too much!";
   }
-    else return str ="Please enter data first";
+  else return str ="Please enter data first";
   }
 
 }
-
 
 })();
